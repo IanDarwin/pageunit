d7ec204bc912ae4da271e589379d9d5ffaf59719
IanDarwin <>
2005-02-15 01:13:38 +0000

    Add more tests.

--- src/pageunit/SelfTestReadTag.java
+++ src/pageunit/SelfTestReadTag.java
@@ -14,13 +14,40 @@ import junit.framework.TestCase;
  */
 public class SelfTestReadTag extends TestCase {
 	
-	public void testFoo() throws Exception {
-		String theURL = "<html><head><foo><bar></head><body><a href='http://grelber/' name=grelber>";
-		Reader is = new StringReader(theURL);
+	String htmlText = "<html><head><foo><bar></head><body><a href='http://grelber/' name=grelber>";
+	
+	public void testReadAll() throws Exception {
+		Reader is = new StringReader(htmlText);
 		ReadTag red = new ReadTag(is);
 		List readTags = red.readTags();
 		assertNotNull("list from readTags", readTags);
 		assertTrue("any tags from readTags", 6 == readTags.size());
+		Iterator tagsIterator = readTags.iterator();
+		while (tagsIterator.hasNext()) {
+			System.out.println(tagsIterator.next());
+		}
+	}
+	
+	public void testReadSome() throws Exception {
+		Reader is = new StringReader(htmlText);
+		ReadTag red = new ReadTag(is);
+		red.setWantedTags(new String[] { "a", "foo" });
+		List readTags = red.readTags();
+		assertNotNull("list from readTags", readTags);
+		assertTrue("any tags from readTags", 2 == readTags.size());
+		Iterator tagsIterator = readTags.iterator();
+		while (tagsIterator.hasNext()) {
+			System.out.println(tagsIterator.next());
+		}
+	}
+	
+	public void testReadNone() throws Exception {
+		Reader is = new StringReader(htmlText);
+		ReadTag red = new ReadTag(is);
+		red.setWantedTags(new String[] { });
+		List readTags = red.readTags();
+		assertNotNull("list from readTags", readTags);
+		assertTrue("any tags from readTags", 0 == readTags.size());
 		Iterator tagsIterator = readTags.iterator();
 		while (tagsIterator.hasNext()) {
 			System.out.println(tagsIterator.next());
