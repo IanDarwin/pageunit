31d89fa712638ccccba3428d8af008d17c606715
IanDarwin <>
2005-05-18 20:07:09 +0000

    Remove one TCP-specific call.

--- src/pageunit/BasicServerTest.java
+++ src/pageunit/BasicServerTest.java
@@ -22,12 +22,10 @@ public class BasicServerTest extends TestCase {
 		String host = TestUtils.getProperty("host");
 		int port = TestUtils.getIntProperty("port");
 		
-		HtmlPage page = TestUtils.getSimplePage(session, host, port, "/index.jsp");
+		HtmlPage page = TestUtils.getSimplePage(session, host, port, "/");
 		
 		WebResponse resp = page.getWebResponse();
-		assertEquals("index page load", 200, resp.getStatusCode());
-		
-		assertTrue("contains title", TestUtils.checkResultForPattern(resp.getContentAsString(),
-				"Toronto Centre for Phenogenomics"));
+		assertTrue("index page either loaded or redirected", resp.getStatusCode() < 400);
+
 	}
 }
