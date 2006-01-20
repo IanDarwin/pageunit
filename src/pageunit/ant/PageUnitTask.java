ba8bb10a40081268f30cf8f27f16694cf51797ee
IanDarwin <>
2006-01-20 21:00:19 +0000

    Placeholder, to remind me to create an Ant Task for running PageUnit tests.

---
+++ src/pageunit/ant/PageUnitTask.java
@@ -0,0 +1,12 @@
+package pageunit.ant;
+
+/**
+ * Placeholder, to remind me to create an Ant Task for running PageUnit tests.
+ */
+public class AntTask {
+
+	public AntTask() {
+		super();
+	}
+
+}
