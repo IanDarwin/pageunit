d8c9e688dc318ec1485b7c2028cc3937307560f4
IanDarwin <>
2006-01-20 22:06:44 +0000

    Add framework support for nascent Ant task.

--- src/pageunit/ant/PageUnitTask.java
+++ src/pageunit/ant/PageUnitTask.java
@@ -1,12 +1,26 @@
 package pageunit.ant;
 
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.taskdefs.Ant;
+
 /**
  * Placeholder, to remind me to create an Ant Task for running PageUnit tests.
  */
-public class AntTask {
+public class AntTask extends Ant {
 
 	public AntTask() {
 		super();
 	}
+	
+	@Override
+	public void execute() throws BuildException {
+		// TODO Auto-generated method stub
+		super.execute();
+	}
 
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
 }
