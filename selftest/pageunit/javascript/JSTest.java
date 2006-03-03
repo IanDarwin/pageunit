6bc2dcf9e5bfbf03162dc23498b04d3dfff47964
IanDarwin <>
2006-03-03 21:46:45 +0000

    Canonicalize the string that the test prints. :-)

--- selftest/pageunit/javascript/JSTest.java
+++ selftest/pageunit/javascript/JSTest.java
@@ -17,7 +17,7 @@ public class JSTest extends TestCase {
 					.getEngineByName("js");
 			System.out.println(engine.getClass());
 			Object ret;
-			ret = engine.eval("print('Hello')");
+			ret = engine.eval("print('Hello World!')");
 			System.out.println("Results: " + ret);
 		} catch (Exception e) {
 			e.printStackTrace();
