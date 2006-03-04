df483e5c85ff95c04edb3526fbc4c2f6e12292f6
IanDarwin <>
2006-03-04 00:28:03 +0000

    Start building tests for all kinds of things that might get created by the Factory; an arry of types.

--- selftest/pageunit/html/HTMLComponentFactoryTest.java
+++ selftest/pageunit/html/HTMLComponentFactoryTest.java
@@ -1,14 +1,43 @@
 package regress.html;
 
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.html.HTML;
+
 import junit.framework.TestCase;
+import pageunit.html.HTMLComponent;
+import pageunit.html.HTMLComponentFactory;
+import pageunit.html.HTMLInputImpl;
+import pageunit.html.HTMLScriptImpl;
+
 
 public class HTMLComponentFactoryTest extends TestCase {
 
+	Object[][] data = {
+			{HTML.Tag.INPUT, HTMLInputImpl.class},
+			{HTML.Tag.SCRIPT, HTMLScriptImpl.class},
+			
+	};
+	
 	/*
 	 * Test method for 'pageunit.html.HTMLComponentFactory.create(Tag, MutableAttributeSet)'
 	 */
-	public void testCreate() {
-
+	public void testCreateAll() {
+		for (Object[] d : data) {
+			HTMLComponent c = HTMLComponentFactory.create((HTML.Tag)d[0], null);
+			assertSame(c.getClass(), d[1]);
+		}
+	}
+	
+	/*
+	 * Test method for 'pageunit.html.HTMLComponentFactory.create(Tag, MutableAttributeSet)'
+	 */
+	public void testCreateScript() {
+		HTML.Tag tag = HTML.Tag.SCRIPT;
+		MutableAttributeSet attrs = null;
+		HTMLComponent myTag = HTMLComponentFactory.create(tag, attrs);
+		assertNotNull(myTag);
+		assertSame(myTag.getClass(), HTMLScriptImpl.class);
+		myTag.setBody("if a < b print(c);");
 	}
 
 }
