59eb36d8c49c5043351d53237011eb36728ee3ad
IanDarwin <>
2006-03-09 23:26:23 +0000

    Incorporate more tests.

--- selftest/pageunit/AllTests.java
+++ selftest/pageunit/AllTests.java
@@ -4,8 +4,11 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import regress.html.HTMLComponentBaseTest;
 import regress.html.HTMLComponentFactoryTest;
+import regress.html.HTMLMetaImplTest;
 import regress.html.ParserTest;
 import regress.html.ParserTest2;
+import regress.html.ParserTest3;
+import regress.html.ParserTest4;
 import regress.javascript.JSTest;
 import regress.linkchecker.LinkCheckerTest;
 
@@ -20,8 +23,11 @@ public class AllTests {
 		suite.addTestSuite(TestUtilsTest.class);
 		suite.addTestSuite(HTMLComponentBaseTest.class);
 		suite.addTestSuite(HTMLComponentFactoryTest.class);
+		suite.addTestSuite(HTMLMetaImplTest.class);
 		suite.addTestSuite(ParserTest.class);
 		suite.addTestSuite(ParserTest2.class);
+		suite.addTestSuite(ParserTest3.class);
+		suite.addTestSuite(ParserTest4.class);
 		suite.addTestSuite(JSTest.class);
 		suite.addTestSuite(LinkCheckerTest.class);
 		//$JUnit-END$
