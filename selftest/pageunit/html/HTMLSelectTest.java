1e3feecd5ea890302adf47c0369f4b241837728f
IanDarwin <>
2006-03-14 18:03:12 +0000

    Factor common code into setUp method. Add test for "bad" input.

--- selftest/pageunit/html/HTMLSelectTest.java
+++ selftest/pageunit/html/HTMLSelectTest.java
@@ -5,16 +5,33 @@ import pageunit.html.*;
 import junit.framework.TestCase;
 
 public class HTMLSelectTest extends TestCase {
-	public void testOne() throws Exception {
-		HTMLSelect sel = new HTMLSelectImpl("province");
-		HTMLOption opt;
-		String[] provinceNames = new String[] { "Ontario", "Quebec" };
+	protected HTMLSelect sel;
+	protected HTMLOption opt;
+	protected String[] provinceNames = new String[] { "Ontario", "Quebec" };
+	
+	@Override
+	protected void setUp() throws Exception {
+		sel = new HTMLSelectImpl("province");
 		for (String s : provinceNames) {
 			opt = new HTMLOptionImpl(s);
 			sel.addChild(opt);
 		}
+	}
+	
+	public void testOne() throws Exception {
+		
 		assertEquals(provinceNames[0], sel.getChildren().get(0).getName());
+		sel.setValue(provinceNames[0]);
 		sel.setValue(provinceNames[1]);
-		assertEquals(provinceNames[1], sel.getValue());
+		assertEquals(provinceNames[1], sel.getValue());		
+	}
+	
+	public void testBadValue() throws Exception {
+		String badValue = "Ohio";
+		
+		// This currently should generate a warning.
+		// Once we get javascript working well, it will throw an IAE.
+		sel.setValue(badValue);
+		assertEquals(badValue, sel.getValue());
 	}
 }
