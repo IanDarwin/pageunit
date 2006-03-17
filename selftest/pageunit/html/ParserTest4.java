653850ead9a448a61472450e796d4068333086bf
IanDarwin <>
2006-03-17 21:28:16 +0000

    Test to be sure that Style and Script and Meta all get found. Check some of them (not yet all!) for body content.

--- selftest/pageunit/html/ParserTest4.java
+++ selftest/pageunit/html/ParserTest4.java
@@ -10,10 +10,14 @@ import pageunit.html.HTMLMeta;
 import pageunit.html.HTMLPage;
 import pageunit.html.HTMLParseException;
 import pageunit.html.HTMLParser;
+import pageunit.html.HTMLScript;
+import pageunit.html.HTMLStyle;
 
 /** Test handling of META redirects */
 public class ParserTest4 extends TestCase {
 	final String testData = "<html><head><title>AIS - Index</title>" + 
+	"<style>A.navbar1HREFSTYLE{font-family: Verdana, Arial, Helvetica, sans-serif;}</style>" +
+	"<SCRIPT LANGUAGE=JavaScript1.2>var highlight; var highlightSelected;</script>" +
 	"<META HTTP-EQUIV=\"Refresh\" content=\"0; URL=barcode_list.jsp\">" +
 	"<body><form action='/foo' method='post' onSubmit='crazy eights'>" +
 	"<input type='submit' name='submit'/></form>" +
@@ -26,18 +30,46 @@ public class ParserTest4 extends TestCase {
 		page = new HTMLParser().parse(new StringReader(testData));
 	}
 	
-	public void testScript() throws Throwable {
+	public void testFindMeta() throws Throwable {
+		System.out.println("ParserTest4.testFindMeta()");
 		assertNotNull(page);
 		boolean found = false;
 		List<HTMLComponent> children = page.getChildren();
 		
 		for (HTMLComponent c : children) {
-			// System.out.println(c);
 			if (c instanceof HTMLMeta) {
 				found = true;
 				System.out.println("Found: " + c);
 			}
 		}
 		assertTrue("Look for meta element", found);
+	}
+	
+	public void testFindScript() throws Throwable {
+		System.out.println("ParserTest4.testFindScript()");
+		assertNotNull(page);
+		List<HTMLComponent> children = page.getChildren();
+		
+		for (HTMLComponent c : children) {
+			if (c instanceof HTMLScript) {
+				System.out.println("Found: " + c);
+				// assertEquals("var highlight; var highlightSelected;", c.getBody());
+				System.err.println("WARNING: test incomplete here");
+				return;
+			}
+		}
+	}
+	
+	public void testFindStyle() throws Throwable {
+		System.out.println("ParserTest4.testFindStyle()");
+		List<HTMLComponent> children = page.getChildren();
+		
+		for (HTMLComponent c : children) {
+			if (c instanceof HTMLStyle) {
+				System.out.println("Found: " + c);
+				assertEquals("A.navbar1HREFSTYLE{font-family: Verdana, Arial, Helvetica, sans-serif;}", c.getBody());
+				return;
+			}
+		}
 	}
 }
