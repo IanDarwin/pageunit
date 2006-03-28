d18c731b62de3da56b4d56bd76a3d3ffc4af6c73
IanDarwin <>
2006-03-28 22:51:26 +0000

    New: test the component stacks.

---
+++ selftest/pageunit/html/ParserComponentStackTest.java
@@ -0,0 +1,18 @@
+package regress.html;
+
+import pageunit.html.GenericHTMLContainer;
+import pageunit.html.HTMLHTMLImpl;
+import pageunit.html.HTMLParser;
+import junit.framework.TestCase;
+
+public class ParserComponentStackTest extends TestCase {
+	public void test() throws Exception {
+		HTMLParser p = new HTMLParser();
+		
+		// p.pushComponent(new HTMLHTMLImpl("MyHTMLImpl"));
+		                                  
+		// p.pushComponent(new GenericHTMLContainer(null, "head"));
+		                                  
+		// p.pushComponent(new GenericHTMLContainer(null, "body"));
+	}
+}
