cc2ff47f181b68b2dc1074e2827e0fe88710fca2
IanDarwin <>
2006-03-30 19:39:22 +0000

    Test that all wanted tags are known correctly to both create() and classForTag().

--- selftest/pageunit/html/HTMLComponentFactoryTest.java
+++ selftest/pageunit/html/HTMLComponentFactoryTest.java
@@ -2,29 +2,61 @@ package regress.html;
 
 import javax.swing.text.MutableAttributeSet;
 import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTML.Tag;
 
 import junit.framework.TestCase;
 import pageunit.html.HTMLComponent;
 import pageunit.html.HTMLComponentFactory;
 import pageunit.html.HTMLInputImpl;
+import pageunit.html.HTMLParser;
 import pageunit.html.HTMLScriptImpl;
 
 
 public class HTMLComponentFactoryTest extends TestCase {
 
-	Object[][] data = {
+	final Object[][] data = {
 			{HTML.Tag.INPUT, HTMLInputImpl.class},
 			{HTML.Tag.SCRIPT, HTMLScriptImpl.class},
-			
 	};
 	
 	/*
 	 * Test method for 'pageunit.html.HTMLComponentFactory.create(Tag, MutableAttributeSet)'
 	 */
-	public void testCreateAll() {
+	public void testCreateFew() {
 		for (Object[] d : data) {
-			HTMLComponent c = HTMLComponentFactory.create((HTML.Tag)d[0], null);
+			final HTMLComponent c = HTMLComponentFactory.create((HTML.Tag)d[0], null);
 			assertSame(c.getClass(), d[1]);
+		}
+	}
+	
+	public void testForBadTag() {
+		try {
+			HTMLComponentFactory.create(HTML.Tag.BR, null);
+			fail("Did not throw exception creating BR");
+		} catch (IllegalStateException e) {
+			System.out.println("Caught expected exception");
+		}
+	}
+	
+	public void testForBadClass() {
+		final Tag tag = HTML.Tag.BR;
+		final Class<?> cl = HTMLComponentFactory.classForTagType(tag);
+		System.out.printf("HTML.Tag %s created class %s%n", tag, cl);
+	}
+	
+	/** Confirm that at least for each tag in WantedTags lists, 
+	 * create() and classForTag() give same type.
+	 */
+	public void testCreateWanted() {
+		for (HTML.Tag t : HTMLParser.wantedSimpleTags) {
+			HTMLComponent comp = HTMLComponentFactory.create(t, null);
+			Class<?> cl = HTMLComponentFactory.classForTagType(t);
+			assertSame(comp.getClass(), cl);
+		}
+		for (HTML.Tag t : HTMLParser.wantedComplexTags) {
+			HTMLComponent comp = HTMLComponentFactory.create(t, null);
+			Class<?> cl = HTMLComponentFactory.classForTagType(t);
+			assertSame(comp.getClass(), cl);
 		}
 	}
 	
