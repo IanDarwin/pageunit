7ebb724d717078fe89dc17abdbf411018b8c11af
IanDarwin <>
2006-04-06 18:50:39 +0000

    Un-comment code that was waiting for move to pageunit.* packages; this removes several warnings.

--- selftest/pageunit/html/ParserComponentStackTest.java
+++ selftest/pageunit/html/ParserComponentStackTest.java
@@ -6,13 +6,14 @@ import pageunit.html.HTMLParser;
 import junit.framework.TestCase;
 
 public class ParserComponentStackTest extends TestCase {
+	
 	public void test() throws Exception {
 		HTMLParser p = new HTMLParser();
 		
-		// p.pushComponent(new HTMLHTMLImpl("MyHTMLImpl"));
+		p.pushComponent(new HTMLHTMLImpl("MyHTMLImpl"));
 		                                  
-		// p.pushComponent(new GenericHTMLContainer(null, "head"));
+		p.pushComponent(new GenericHTMLContainer(null, "head"));
 		                                  
-		// p.pushComponent(new GenericHTMLContainer(null, "body"));
+		p.pushComponent(new GenericHTMLContainer(null, "body"));
 	}
 }
