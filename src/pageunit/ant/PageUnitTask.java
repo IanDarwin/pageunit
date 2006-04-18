81d4785b68a3c73b5b4a68f63922c0b307b6c260
IanDarwin <>
2006-04-18 19:36:14 +0000

    More work on the Ant task.

--- src/pageunit/ant/PageUnitTask.java
+++ src/pageunit/ant/PageUnitTask.java
@@ -1,26 +1,64 @@
 package pageunit.ant;
 
+import java.io.File;
+
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.taskdefs.Ant;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.Path;
 
 /**
- * Placeholder, to remind me to create an Ant Task for running PageUnit tests.
+ * Simple Ant Task for running PageUnit tests.
+ * Usage Prerequisite:
+ * junit and pageunit (and their depends) on classpath.
+ * &lt;taskdef name="pageunit" classname="pageunit.ant.AntTask"&gt;
+ * Usage examples: 
+ * &lt;pageunit file="/foo.bar/test.txt"/&gt;
+ * &lt;pageunit testPath="dir1:dir2" test="test1.txt"/&gt;
+ * &lt;pageunit dir="/foo.bar"/&gt;
  */
-public class AntTask extends Ant {
+public class AntTask extends Task {
 
-	public AntTask() {
-		super();
-	}
+	private File theFile;
+	private String theTest;
+	private Path thePath;
 	
 	@Override
 	public void execute() throws BuildException {
-		// TODO Auto-generated method stub
-		super.execute();
+		if (theFile == null && theTest == null) {
+			throw new BuildException("Either file, or test (and optionally path) must be specified");
+		}
+		if (theFile != null) {
+			// XXX process file
+			return;
+		}
+		if (theTest != null) {
+			if (thePath == null) {
+				// create File(theTest), try to process
+				return;
+			} else {
+				// walk thePath, looking for test, first found, process 
+				return;
+			}
+		}
+		throw new BuildException("Value of fileName must be set");		
 	}
 
-	@Override
-	public void init() {
-		// TODO Auto-generated method stub
-		super.init();
+
+	public void addText(String unwantedText) {
+		throw new BuildException("The PageUnit task does not support nested text");
+	}
+	
+	// --------------- Simple Accessors -------------------
+
+	public void setFile(File fileName) {
+		this.theFile = fileName;
+	}
+	
+	public void setTest(String testName) {
+		this.theTest = testName;
+	}
+	
+	public void setTestPath(Path p) {
+		this.thePath = p;
 	}
 }
