a4be16a5902d264b506fef081f67215b18a0a6a2
IanDarwin <>
2006-04-28 22:49:20 +0000

    Warning removal.

--- src/pageunit/html/HTMLParseException.java
+++ src/pageunit/html/HTMLParseException.java
@@ -2,6 +2,8 @@ package pageunit.html;
 
 public class HTMLParseException extends Exception {
 
+	private static final long serialVersionUID = 5313124205302445808L;
+
 	public HTMLParseException() {
 		super();
 	}
