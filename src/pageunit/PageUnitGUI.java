7644b2d5777af75c332f2bcc85e6a7d0f23e13dc
IanDarwin <>
2006-05-02 18:44:58 +0000

    Add red/green progress bar, "just like the big guys". Remember previous directory for file chooser.

--- src/pageunit/PageUnitGUI.java
+++ src/pageunit/PageUnitGUI.java
@@ -1,5 +1,7 @@
 package pageunit;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -12,8 +14,12 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 
+import junit.framework.AssertionFailedError;
+import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 
@@ -24,21 +30,34 @@ import com.darwinsys.swingui.UtilGUI;
  */
 public class PageUnitGUI extends PageUnit {
 
-	final static JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
+	final JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
 	
-	final static Preferences p = Preferences.userNodeForPackage(PageUnitGUI.class);
+	final Preferences p = Preferences.userNodeForPackage(PageUnitGUI.class);
+	
+	final JProgressBar bar = new JProgressBar();
 	
 	/**
 	 * Main method; ignores arguments.
 	 */
 	public static void main(String[] args) {
+		new PageUnitGUI();
+	}
+	
+	PageUnitGUI() {
 		final JFrame jf = new JFrame("PageUnit Runner");
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		final Container cp = jf.getContentPane();
+		final Container cp = new JPanel();
+		jf.add(cp, BorderLayout.CENTER);
+		
 		cp.setLayout(new FlowLayout());
 		cp.add(new JLabel("PageUnit Test File:"));
 		final JTextField tf = new JTextField(20);
 		cp.add(tf);
+		
+		String rememberedCurrentDirectory = p.get("directory", null);
+		if (rememberedCurrentDirectory != null) {
+			fileChooser.setCurrentDirectory(new File(rememberedCurrentDirectory));
+		}
 
 		final JButton browseButton = new JButton("...");
 		cp.add(browseButton);
@@ -50,6 +69,7 @@ public class PageUnitGUI extends PageUnit {
 				if (ret != JFileChooser.APPROVE_OPTION) {
 					return;
 				}
+				p.put("directory", fileChooser.getCurrentDirectory().getAbsolutePath());
 				tf.setText(fileChooser.getSelectedFile().getAbsolutePath());
 			}		
 		});
@@ -68,11 +88,24 @@ public class PageUnitGUI extends PageUnit {
 					error(jf, "Can't read file " + f);
 				}
 				try {
-					TestResult results = new TestResult();
+					TestResult results = new TestResult() {
+						@Override
+						public void startTest(Test test) {
+							crank();
+						}
+						@Override
+						public synchronized void addError(Test test, Throwable t) {
+							seeRed();
+						}
+						@Override
+						public synchronized void addFailure(Test test, AssertionFailedError t) {
+							seeRed();
+						}
+					};
 					TestCase t = new ScriptTestCase(f.getAbsolutePath());
-					int max = results.runCount();
-					System.out.printf("Out of %d runs%n", max);
-					
+					int max = t.countTestCases();
+					System.out.printf("Starting %d tests%n", max);
+					bar.setMaximum(max);
 					t.run(results);
 				} catch (Exception e) {
 					error(jf, e.toString());
@@ -80,10 +113,30 @@ public class PageUnitGUI extends PageUnit {
 				}
 			}			
 		});		
+		jf.add(bar, BorderLayout.SOUTH);
+		seeGreen();
 		
 		jf.pack();
 		UtilGUI.monitorWindowPosition(jf, p);
 		jf.setVisible(true);
+	}
+	
+	/**
+	 * Set the bar to green, used only at the beginning
+	 */
+	void seeGreen() {
+		bar.setForeground(Color.GREEN);
+	}
+	/**
+	 * Set the bar to red, used when a test fails or errors.
+	 */
+	void seeRed() {
+		bar.setForeground(Color.RED);
+	}
+	
+	void crank() {
+		int x = bar.getValue();
+		bar.setValue(++x);
 	}
 	
 	static void error(final JFrame tf, String mesg) {
