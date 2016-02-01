16250fd1502ba2dcad934c76919cd167505f4ab7
Ian <ian@darwinsys.com>
2016-01-31 21:13:12 -0500

    Don't import what you don't even use.

--- src/main/java/pageunit/http/WebRequest.java
+++ src/main/java/pageunit/http/WebRequest.java
@@ -3,16 +3,11 @@ package pageunit.http;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.Cookie;
-
 /** Similar to an HttpServletRequest, but lighter */
 public class WebRequest {
 	
 	private Map<String, String> headerMap = new HashMap<String,String>();
 
-	public Cookie getCookie(String name) {
-		return null;
-	}
 	public void setHeader(String name, String value) {
 		headerMap.put(name, value);
 	}
