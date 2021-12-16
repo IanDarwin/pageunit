0259bdd0e005b76f3007b482bc7619ea15328ba7
Ian Darwin <ian@darwinsys.com>
2021-12-16 14:31:54 -0500

    Don't fail test if js engine missing

--- src/test/java/pageunit/javascript/JSTest.java
+++ src/test/java/pageunit/javascript/JSTest.java
@@ -6,15 +6,26 @@ import javax.script.ScriptEngineManager;
 import junit.framework.TestCase;
 
 /**
- * Simple test to ensure that JavaScript engine is working
- * 
+ * Simple test to ensure that JavaScript engine is working.
+ * Or at least isn't failing, if present.
+ * "Engine not found" should not be counted as a failure.
  * @author ian
  */
 public class JSTest extends TestCase {
 	public static void testJavaScript() throws Exception {
 		try {
-			ScriptEngine engine = new ScriptEngineManager()
-					.getEngineByName("js");
+			final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+			String js = "js";
+			ScriptEngine engine = scriptEngineManager
+					.getEngineByName(js);
+			if (engine == null) {
+				System.out.println("Cannot load JavaScript ScriptEngine " + js);
+				System.out.println("These engines found:");
+				System.out.println("----------");
+				scriptEngineManager.getEngineFactories().forEach(System.out::println);
+				System.out.println("----------");
+				return;
+			}
 			System.out.println(engine.getClass());
 			Object ret;
 			ret = engine.eval("print('Hello World!')");
